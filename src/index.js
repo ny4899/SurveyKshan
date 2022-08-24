7542a6fcd3924c5a16c9dbc1714de9eea51ccf02
ny4899 <ny4899@gmail.com>
2022-08-24 15:30:39 +0530

    added hashroute

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 
 // bootstarp
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
@@ -13,8 +13,8 @@ import "./index.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
